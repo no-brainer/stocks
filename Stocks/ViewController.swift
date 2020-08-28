0b6d695e68c4f948c13a29787dad41d9c5a62a63
Polina Guseva <p.guseva00@gmail.com>
2020-08-28 23:11:47 +0300

    Initial Commit

---
+++ Stocks/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Stocks
+//
+//  Created by Polina A. Guseva on 28.08.2020.
+//  Copyright © 2020 Polina A. Guseva. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
